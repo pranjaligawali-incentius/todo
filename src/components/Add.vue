fd2d25660f421712ccc9fd30d61e3402e32ecf28
pranjaligawali-incentius <pranjali.gawali@incentius.com>
2026-03-13 17:47:53 +0530

    Updated correctly

--- src/components/Add.vue
+++ src/components/Add.vue
@@ -3,13 +3,23 @@ import { ref } from 'vue'
 
 const name = ref('');
 const nameList = ref([]); // array to store names
+const editingIndex=ref(null);
 
 function add(){
+  console.log("I am here");
   if(name.value.trim() !==''){
     nameList.value.push(name.value);
     name.value='';
   }
 }
+function edit(){
+  if(name.value.trim() !==''){
+    nameList.value[editingIndex.value]=name.value;
+    name.value='';
+    editingIndex.value=null;
+  }
+}
+
 
 // Function to delete an item by its index
 function deleteItem(index) {
@@ -20,17 +30,21 @@ function editItem(index) {
   name.value = nameList.value[index]; // Populate input with current name
   editingIndex.value = index; // Set the index of the item being edited
 }
+
 </script>
 
-<<template>
+<template>
   <div>
     <h2>Add/Edit Student</h2>
     <input v-model="name" placeholder="Enter a name" @keyup.enter="add">
     <!-- Change button text based on editing mode -->
-    <button @click="add">
+    <!-- <button @click="editingIndex !== null ? edit() : add()">
       {{ editingIndex !== null ? 'Save Changes' : 'Add Name' }}
-    </button>
+    </button> -->
+    <button v-if="editingIndex==null" @click="add">Add</button>
+    <button v-else @click="edit">Save Changes</button>
 
+    
     <ul>
       <li v-for="(item, index) in nameList" :key="index">
         {{ item }}
@@ -42,5 +56,4 @@ function editItem(index) {
     </ul>
   </div>
 </template>
-
 
